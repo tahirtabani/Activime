b9c6e407c8f9e3f509781a1879bcafc2bcf43bb6
ZalwayJH <joelhaughton94@gmail.com>
2022-11-14 17:22:42 +0000

    starting on getting info from db

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Home = () => {
+  db.collection("activity")
+    .get("7mrlJGZrjvyBVyrwGqiU")
+    .then((data) => {
+      console.log(data, "data from get");
+    })
+    .catch((err) => {
+      console.log(err, "error message");
+    });
+
+  // .then((querySnapshot) => {
+  //   querySnapshot.docs.forEach((doc) => {
+  //     markers.push(doc.data());
+  //   });
+  // });
+
   return (
     <View>
       <Text style={{ fontSize: 100 }}>Home</Text>
