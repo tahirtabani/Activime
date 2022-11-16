decde713eeae910c1853e1ccce3a595937e01af7
ZalwayJH <joelhaughton94@gmail.com>
2022-11-16 12:16:58 +0000

    made it so clicking on an individual card only expands that card

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,6 +11,8 @@ import { db } from "../../firebase";
 
 const Home = () => {
   const [activity, setActivity] = useState([]);
+  const [expand, setExpanded] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
 
   const activityRef = db.collection("activity");
 
@@ -34,23 +36,68 @@ const Home = () => {
     });
   }, []);
 
+  // const handlePressOnCard = () => {
+  //   expand === false ? setExpanded(true) : setExpanded(false);
+
+  //   return (
+  //     <Pressable style={styles.expandedCard} onPressOut={handlePressOnCard}>
+  //       <View>
+  //         <Image source={{ uri: activity.imageUrl }} style={styles.image} />
+  //       </View>
+  //       <View style={styles.cardDetails}>
+  //         <Text style={{ fontWeight: "bold" }}>{activity.title}</Text>
+  //         <Text>{activity.area}</Text>
+  //         <Text>{activity.user}</Text>
+  //         <Text>{activity.description}</Text>
+  //       </View>
+  //     </Pressable>
+  //   );
+  // };
+
   return (
     <View style={styles.border}>
       <FlatList
         data={activity}
         numColumns={1}
-        renderItem={({ item }) => (
-          <Pressable style={styles.card}>
-            <View>
-              <Image source={{ uri: item.imageUrl }} style={styles.image} />
-            </View>
-            <View style={styles.cardDetails}>
-              <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
-              <Text>{item.area}</Text>
-              <Text>{item.user}</Text>
-            </View>
-          </Pressable>
-        )}
+        renderItem={({ item }) =>
+          item.id === selectedId ? (
+            <Pressable
+              style={styles.card}
+              onPressOut={() => {
+                setSelectedId(item.id);
+                console.log(item.id);
+              }}
+            >
+              <View>
+                <Image source={{ uri: item.imageUrl }} style={styles.image} />
+              </View>
+              <View style={styles.cardDetails}>
+                <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
+                <Text>{item.area}</Text>
+                <Text>{item.user}</Text>
+                <Text>{item.description}</Text>
+              </View>
+            </Pressable>
+          ) : (
+            <Pressable
+              style={styles.card}
+              onPressOut={() => {
+                setSelectedId(item.id);
+              }}
+            >
+              <View>
+                <Image source={{ uri: item.imageUrl }} style={styles.image} />
+              </View>
+              <View style={styles.cardDetails}>
+                <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
+                <Text>{item.area}</Text>
+                <Text>{item.user}</Text>
+              </View>
+            </Pressable>
+          )
+        }
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
       ></FlatList>
     </View>
   );
@@ -85,5 +132,13 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     justifyContent: "space-evenly",
     flexShrink: 1,
+  },
+  expandedCard: {
+    backgroundColor: "#B7B5BD",
+    margin: 10,
+    borderRadius: 18,
+    flexDirection: "row",
+    shadowColor: "#000000",
+    elevation: 20,
   },
 });
