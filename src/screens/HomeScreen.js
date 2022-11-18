8e72944bbfb2be807541bc20a0d70bfbd3a780ec
ZalwayJH <joelhaughton94@gmail.com>
2022-11-18 10:57:33 +0000

    fixed missing bracket

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -12,18 +12,14 @@ import React, { useState, useEffect } from "react";
 import { db } from "../../firebase";
 import ActivityList from "./ActivityList";
 
-
 const HomeScreen = () => {
-
   return (
     <View>
       <ActivityList />
     </View>
   );
-
+};
 
 export default HomeScreen;
 
-
 const styles = StyleSheet.create({});
-
