c3b209faaaa11980f8b951182f3c71462b45b297
stan2712 <staann27@gmail.com>
2022-11-24 13:56:51 +0000

    small changes to card

--- src/screens/ActivityList.js
+++ src/screens/ActivityList.js
@@ -75,7 +75,8 @@ const ActivityList = () => {
               <View style={styles.cardDetailsContainer}>
                 <Text style={styles.cardTitle}>{item.title}</Text>
                 <Text style={styles.cardDetails}>{item.area}</Text>
-                <Text style={styles.cardDetails}>{item.time}</Text>
+                <Text style={styles.cardDetails}>{item.date}</Text>
+                <Text style={styles.cardDetails}>Created by: {item.user}</Text>
               </View>
             </Pressable>
           ) : (
