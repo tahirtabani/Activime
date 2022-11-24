f52ca69b87533473dfc95eca6913666085dee986
Nick Jackson <ncj0110@protonmail.com>
2022-11-24 14:10:30 +0000

    1

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -8,8 +8,8 @@ import {
   snapshot,
   connectFirestoreEmulator,
   GeoPoint,
-} from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+} from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 import {
   StatusBar,
   StyleSheet,
@@ -21,19 +21,19 @@ import {
   KeyboardAvoidingView,
   Button,
   Pressable,
-} from "react-native";
-import MapButton from "./MapButton";
+} from 'react-native';
+import MapButton from './MapButton';
 // import DateTimePicker from "@react-native-community/datetimepicker";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-import { Formik, validateYupSchema } from "formik";
-import * as Yup from "yup";
-import { db } from "../../firebase";
-import * as _ from "lodash";
-import React, { useState, useEffect } from "react";
-import DatePicker from "react-native-modern-datepicker";
+import { Formik, validateYupSchema } from 'formik';
+import * as Yup from 'yup';
+import { db } from '../../firebase';
+import * as _ from 'lodash';
+import React, { useState, useEffect } from 'react';
+import DatePicker from 'react-native-modern-datepicker';
 
 // const deleteEmptyMessages = async () => {
 //   console.log("Im in");
@@ -57,9 +57,9 @@ import DatePicker from "react-native-modern-datepicker";
 // };
 const SignupSchema = Yup.object().shape({
   title: Yup.string()
-    .min(6, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Please enter a title"),
+    .min(6, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Please enter a title'),
   // date: Yup.string()
   //   .min(9, "Too Short!")
   //   .max(9, "Too Long!")
@@ -93,10 +93,10 @@ const PostScreen = ({ navigation }) => {
     hideDatePicker();
   };
 
-  console.log(finalLocation, "final location");
-  console.log(chosenLocation, "chosen location");
+  console.log(finalLocation, 'final location');
+  console.log(chosenLocation, 'chosen location');
   const handleConfirmTime = (time) => {
-    console.warn("time: ", time);
+    console.warn('time: ', time);
 
     // console.log("selectedDate: ", selectedDate.toLocaleDateString());
     hideDatePicker();
@@ -108,30 +108,33 @@ const PostScreen = ({ navigation }) => {
   }, []);
 
   const AddData = (data) => {
-    console.log("data: ", data);
-    const ref = collection(db, "activity");
+    console.log('data: ', data);
+    const ref = collection(db, 'activity');
 
     addDoc(ref, data)
       .then((ref) => {
-        alert("Activity posted successfully");
-        navigation.navigate("Tabs");
+        alert('Activity posted successfully');
+        navigation.navigate('Tabs');
       })
       .catch((error) => {
         console.log(error);
       });
   };
   return (
-    <KeyboardAvoidingView behavior="height">
+    <KeyboardAvoidingView
+      behavior='padding'
+      style={{ backgroundColor: '#3F3947' }}
+    >
       <Formik
         initialValues={{
-          title: "",
+          title: '',
           date: selectedDate.toLocaleDateString(),
           time: selectedTime,
-          description: "",
-          imageUrl: "",
-          area: "",
-          location: "",
-          user: "",
+          description: '',
+          imageUrl: '',
+          area: '',
+          location: '',
+          user: '',
         }}
         validationSchema={SignupSchema}
         onSubmit={(values) => {
@@ -156,18 +159,18 @@ const PostScreen = ({ navigation }) => {
                   <TextInput
                     style={{ height: 0 }}
                     placeholder={getAuth().currentUser.displayName}
-                    placeholderTextColor="#FFFF"
-                    onChangeText={handleChange("user")}
+                    placeholderTextColor='#FFFF'
+                    onChangeText={handleChange('user')}
                     value={(values.user = getAuth().currentUser.displayName)}
                   />
                 </View>
                 <TextInput
                   style={styles.inputStyle}
-                  placeholder="Title..."
-                  placeholderTextColor="#FFFF"
-                  onChangeText={handleChange("title")}
+                  placeholder='Title...'
+                  placeholderTextColor='#FFFF'
+                  onChangeText={handleChange('title')}
                   value={values.Title}
-                  onBlur={() => setFieldTouched("title")}
+                  onBlur={() => setFieldTouched('title')}
                 />
                 {errors.title && (
                   <Text style={styles.errorTxt}>{errors.title}</Text>
@@ -178,15 +181,15 @@ const PostScreen = ({ navigation }) => {
                 <View style={styles.dateContainer}>
                   <TextInput
                     style={styles.dateInputStyle}
-                    placeholder={"Date"}
-                    onChangeText={handleChange("date")}
+                    placeholder={'Date'}
+                    onChangeText={handleChange('date')}
                     value={selectedDate.toLocaleDateString()}
                   />
                   <Pressable onPress={showDatePicker} style={styles.dateButton}>
                     <FontAwesome
-                      name="calendar"
+                      name='calendar'
                       size={30}
-                      color="#fff"
+                      color='#fff'
                       style={styles.buttonIcon}
                     />
                   </Pressable>
@@ -194,7 +197,7 @@ const PostScreen = ({ navigation }) => {
                   <DateTimePickerModal
                     date={selectedDate}
                     isVisible={isDatePickerVisible}
-                    mode="datetime"
+                    mode='datetime'
                     onConfirm={handleConfirm}
                     onCancel={hideDatePicker}
                   />
@@ -208,15 +211,15 @@ const PostScreen = ({ navigation }) => {
                 <View style={styles.dateContainer}>
                   <TextInput
                     style={styles.dateInputStyle}
-                    placeholder={"Time"}
-                    onChangeText={handleChange("time")}
+                    placeholder={'Time'}
+                    onChangeText={handleChange('time')}
                     value={selectedTime}
                   />
                   <Pressable onPress={showDatePicker} style={styles.dateButton}>
                     <FontAwesome
-                      name="clock-o"
+                      name='clock-o'
                       size={30}
-                      color="#fff"
+                      color='#fff'
                       style={styles.buttonIcon}
                     />
                   </Pressable>
@@ -225,60 +228,60 @@ const PostScreen = ({ navigation }) => {
               <View style={styles.inputWrapper}>
                 <TextInput
                   style={styles.inputStyle}
-                  placeholder="Description..."
+                  placeholder='Description...'
                   multiline={true}
                   minHeight={100}
-                  textAlignVertical="top"
-                  placeholderTextColor="#FFFF"
-                  onChangeText={handleChange("description")}
+                  textAlignVertical='top'
+                  placeholderTextColor='#FFFF'
+                  onChangeText={handleChange('description')}
                   value={values.description}
                 />
               </View>
               <View style={styles.inputWrapper}>
                 <TextInput
                   style={styles.inputStyle}
-                  placeholder="ImageURL"
-                  placeholderTextColor="#FFFF"
-                  onChangeText={handleChange("imageUrl")}
+                  placeholder='ImageURL'
+                  placeholderTextColor='#FFFF'
+                  onChangeText={handleChange('imageUrl')}
                   value={values.imageUrl}
                 />
               </View>
               <View
                 style={{
-                  flexDirection: "column",
-                  width: "85%",
-                  height: "auto",
+                  flexDirection: 'column',
+                  width: '85%',
+                  height: 'auto',
                 }}
               >
                 <TextInput
                   style={styles.inputStyle}
-                  placeholder="Area"
-                  placeholderTextColor="#FFFF"
-                  onChangeText={handleChange("area")}
+                  placeholder='Area'
+                  placeholderTextColor='#FFFF'
+                  onChangeText={handleChange('area')}
                   value={values.area}
                 />
               </View>
 
               <View
                 style={{
-                  flexDirection: "column",
+                  flexDirection: 'column',
                   top: -49,
                   marginLeft: 302,
-                  height: "auto",
+                  height: 'auto',
                   width: 40,
                 }}
               >
                 <TextInput
                   style={{
                     height: 50,
-                    borderColor: "#3F3947",
+                    borderColor: '#3F3947',
                     borderWidth: 1,
                     borderRadius: 10,
-                    backgroundColor: "#446E80",
-                    color: "#fff",
+                    backgroundColor: '#446E80',
+                    color: '#fff',
                   }}
-                  placeholderTextColor="#FFFF"
-                  onChangeText={handleChange("geopoint")}
+                  placeholderTextColor='#FFFF'
+                  onChangeText={handleChange('geopoint')}
                   value={
                     (values.location = new GeoPoint(
                       chosenLocation.latlng.latitude,
@@ -296,7 +299,7 @@ const PostScreen = ({ navigation }) => {
                 disabled={!isValid}
                 style={[
                   styles.submitBtn,
-                  { backgroundColor: isValid ? "#446E80" : "#A5C9CA" },
+                  { backgroundColor: isValid ? '#446E80' : '#A5C9CA' },
                 ]}
               >
                 <Text style={styles.submitBtnTxt}>Submit</Text>
@@ -313,56 +316,56 @@ export default PostScreen;
 
 const styles = StyleSheet.create({
   wrapper: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#3F3947",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3F3947',
     paddingHorizontal: 15,
-    paddingTop: "auto",
-    height: "100%",
+    paddingTop: 'auto',
+    height: '100%',
   },
   inputWrapper: {
     flex: 1,
-    width: "50%",
+    width: '50%',
   },
 
   dateButton: {
-    position: "absolute",
+    position: 'absolute',
     right: 10,
     top: 8,
   },
 
   formContainer: {
-    backgroundColor: "#1C1924",
+    backgroundColor: '#1C1924',
     padding: 20,
     borderRadius: 20,
-    width: "100%",
+    width: '100%',
   },
   dateContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     flex: 1,
   },
   inputStyle: {
-    borderColor: "#3F3947",
+    borderColor: '#3F3947',
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    backgroundColor: "#3F3947",
-    color: "#fff",
+    backgroundColor: '#3F3947',
+    color: '#fff',
   },
   dateInputStyle: {
-    borderColor: "#16213E",
+    borderColor: '#16213E',
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
-    backgroundColor: "#3F3947",
-    color: "#fff",
+    backgroundColor: '#3F3947',
+    color: '#fff',
   },
   title: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 26,
-    fontWeight: "400",
+    fontWeight: '400',
     marginBottom: 15,
-    textAlign: "center",
+    textAlign: 'center',
   },
   inputWrapper: {
     marginBottom: 15,
@@ -370,21 +373,21 @@ const styles = StyleSheet.create({
 
   errorTxt: {
     fontSize: 12,
-    color: "#FF0D10",
+    color: '#FF0D10',
   },
   submitBtn: {
     // backgroundcolor: "#395B64",
     padding: 10,
     borderRadius: 15,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   submitBtnTxt: {
-    color: "#fff",
-    textAlign: "center",
+    color: '#fff',
+    textAlign: 'center',
     fontSize: 18,
-    fontWeight: "700",
+    fontWeight: '700',
   },
   dateContainer: {
-    width: "100%",
+    width: '100%',
   },
 });
