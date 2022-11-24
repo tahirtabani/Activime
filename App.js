2f16fb06d855d00605eb7951357a1a0aecb410bd
Nick Jackson <ncj0110@protonmail.com>
2022-11-24 17:40:29 +0000

    finish style

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import LoginScreen from "./src/screens/LoginScreen";
-import RegistrationScreen from "./src/screens/RegistrationScreen";
-import HomeScreen from "./src/screens/HomeScreen";
-import ChatScreen from "./src/screens/Chat";
-import Tabs from "./src/screens/Tabs";
-import PostScreen from "./src/screens/PostScreen";
-import SavedScreen from "./src/screens/SavedScreen";
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './src/screens/LoginScreen';
+import RegistrationScreen from './src/screens/RegistrationScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import ChatScreen from './src/screens/Chat';
+import Tabs from './src/screens/Tabs';
+import PostScreen from './src/screens/PostScreen';
+import SavedScreen from './src/screens/SavedScreen';
 
-import MapComponent from "./src/screens/MapComponent";
-import MapScreen from "./src/screens/MapScreen";
+import MapComponent from './src/screens/MapComponent';
+import MapScreen from './src/screens/MapScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -20,7 +20,7 @@ export default function App() {
       <View style={styles.logoContainer}>
         <Image
           style={styles.logo}
-          source={require("./Images/activime_logo_white.png")}
+          source={require('./Images/activime_logo_white.png')}
         ></Image>
       </View>
     );
@@ -28,63 +28,63 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ animation: "none" }}>
-        <Stack.Group options={{ headerStyle: { backgroundColor: "#1C1924" } }}>
+      <Stack.Navigator screenOptions={{ animation: 'none' }}>
+        <Stack.Group options={{ headerStyle: { backgroundColor: '#1C1924' } }}>
           <Stack.Screen
-            name="LoginScreen"
+            name='LoginScreen'
             component={LoginScreen}
             options={{ headerShown: false }}
           />
           <Stack.Screen
-            name="Tabs"
+            name='Tabs'
             component={Tabs}
             options={{
-              headerStyle: { backgroundColor: "#446E80" },
-              headerTintColor: "#fff",
+              headerStyle: { backgroundColor: '#446E80' },
+              headerTintColor: '#fff',
               headerTitle: displayLogo,
             }}
           />
           <Stack.Screen
-            name="PostScreen"
+            name='PostScreen'
             component={PostScreen}
             options={{
-              headerStyle: { backgroundColor: "#1C1924" },
-              headerTintColor: "#fff",
+              headerStyle: { backgroundColor: '#1C1924' },
+              headerTintColor: '#fff',
               headerTitle: displayLogo,
             }}
           />
 
           <Stack.Screen
-            name="HomeScreen"
+            name='HomeScreen'
             component={HomeScreen}
             options={{
-              headerStyle: { backgroundColor: "#1C1924" },
-              headerTintColor: "#fff",
+              headerStyle: { backgroundColor: '#1C1924' },
+              headerTintColor: '#fff',
               headerTitle: displayLogo,
             }}
           />
           <Stack.Screen
-            name="RegistrationScreen"
+            name='RegistrationScreen'
             component={RegistrationScreen}
             options={{ headerShown: false }}
           />
 
           <Stack.Screen
-            name="ChatScreen"
+            name='ChatScreen'
             component={ChatScreen}
             options={{
-              headerStyle: { backgroundColor: "#1C1924" },
-              headerTintColor: "#fff",
+              headerStyle: { backgroundColor: '#1C1924' },
+              headerTintColor: '#fff',
               headerTitle: displayLogo,
             }}
           />
 
           <Stack.Screen
-            name="SavedScreen"
+            name='SavedScreen'
             component={SavedScreen}
             options={{
-              headerStyle: { backgroundColor: "#1C1924" },
-              headerTintColor: "#fff",
+              headerStyle: { backgroundColor: '#1C1924' },
+              headerTintColor: '#fff',
               headerTitle: displayLogo,
             }}
           />
@@ -97,10 +97,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    textColor: "white",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    textColor: 'white',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logoContainer: {
     marginVertical: 10,
@@ -109,11 +109,11 @@ const styles = StyleSheet.create({
   },
 
   logo: {
-    resizeMode: "contain",
+    resizeMode: 'contain',
     flex: 1,
     width: 200,
     height: 50,
-    alignItems: "center",
-    marginLeft: 20,
+    alignItems: 'center',
+    marginLeft: 32,
   },
 });
